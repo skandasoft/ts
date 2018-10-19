8b7f3252b28eb4c314b147feb018cfbbf12fe097
Skandasoft <skandasoftinc@gmail.com>
2018-10-19 14:44:00 -0500

    first commit

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+console.log("hello world");
+console.log("Debug");
+var array = ["manic", "goapal"];
+
+for (var index = 0; index < array.length; index++) {
+  var element = array[index];
+  console.log(element);
+}
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+console.log("hello world");
+console.log("Debug");
+let array: string[] = [
+	"manic",
+	"goapal",
+];
+for (let index = 0; index < array.length; index++) {
+	const element = array[index];
+	console.log(element);
+}
